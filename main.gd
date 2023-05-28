3c3629a918618a0024a88672336f04ffd674cf91
cowwabanga <114105506+cowwabanga@users.noreply.github.com>
2023-05-28 22:53:30 +0500

    8 shots, and everything over
    you better reload this shotgun as fast as you can...

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node2D
 var bullet : PackedScene = preload("res://bullet.tscn")
 var crosshair : Sprite2D
 var player : CharacterBody2D
+var shotgun_clip : int = 8
 
 func _ready() -> void:
 	crosshair = get_node("crosshair")
@@ -14,9 +15,17 @@ func _process(delta: float) -> void:
 	crosshair.position.y = get_global_mouse_position().y
 	
 	if Input.is_action_just_pressed("lmb"):
-		print("Shot fired!")
-		var bullet_instance = bullet.instantiate()
-		# Create bullet instance, set it rotation to the crosshair position, and add it to the scene
-		bullet_instance.position = Vector2(player.position.x, player.position.y)
-		bullet_instance.look_at(crosshair.position)
-		add_child(bullet_instance)
+		if (shotgun_clip > 0):
+			var bullet_instance = bullet.instantiate()
+			# Create bullet instance, set it rotation to the crosshair position, and add it to the scene
+			bullet_instance.position = Vector2(player.position.x, player.position.y)
+			bullet_instance.look_at(crosshair.position)
+			add_child(bullet_instance)
+			shotgun_clip -= 1
+			print("Shot fired!")
+		else:
+			print("NO MORE SHELLS LEFT!!!")
+	
+	if Input.is_action_just_pressed("rmb"):
+		shotgun_clip = 8;
+		print("Reloaded!")
